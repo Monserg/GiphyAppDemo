ac3a5040a7f55c737a8f41cb171850830e2a5a5a
SerhiiM <msm72@mail.ru>
2017-11-02 17:33:54 +0200

    create SearchController

---
+++ GiphyAppDemo/Scenes/GIFObjectsShow/Views/GIFCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  GIFCollectionViewCell.swift
+//  GiphyAppDemo
+//
+//  Created by msm72 on 02.11.2017.
+//  Copyright © 2017 RemoteJob. All rights reserved.
+//
+
+import UIKit
+
+class GIFCollectionViewCell: UICollectionViewCell {
+    // MARK: - Properties
+    @IBOutlet weak var importDateTimeLabel: UILabel!
+}
